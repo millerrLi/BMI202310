be035de068f4e3cf6473b8b7c34603b80f80e0d5
millerr <s0950311@gmailcom>
2023-10-26 08:50:37 +0800

    BMI test1

--- app/src/main/java/com/millerr/bmi/MainActivity.kt
+++ app/src/main/java/com/millerr/bmi/MainActivity.kt
@@ -2,10 +2,23 @@ package com.millerr.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.millerr.bmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+
+    fun bmi(view: View){
+        val weight = binding.edWeight.text.toString().toFloat()
+        val height = binding.edHeight.text.toString().toFloat()
+        val bmi = weight / (height * height)
+        Log.d("BMI", bmi.toString())
     }
 }
