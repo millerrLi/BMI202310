74c4657b304f1bbf5e39c088bcad7916c2a37272
millerr <s0950311@gmailcom>
2023-10-30 08:06:51 +0800

    多國語言

--- app/src/main/java/com/millerr/bmi/MainActivity.kt
+++ app/src/main/java/com/millerr/bmi/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import com.millerr.bmi.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -20,5 +22,11 @@ class MainActivity : AppCompatActivity() {
         val height = binding.edHeight.text.toString().toFloat()
         val bmi = weight / (height * height)
         Log.d("BMI", bmi.toString())
+        Toast.makeText(this, bmi.toString(), Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setMessage(bmi.toString())
+            .setPositiveButton("OK", null)
+            .setNeutralButton("Cancel", null)
+            .show()
     }
 }
